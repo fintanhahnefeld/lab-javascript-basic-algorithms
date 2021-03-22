d2fdebf41348fb282ecc4607886ce56333607c56
Fintan Hahnefeld <fintan22@gmail.com>
2021-03-22 19:13:17 +0100

    extra attempts added

--- js/index.js
+++ js/index.js
@@ -15,14 +15,6 @@ if (hacker1.length > hacker2.length) {
 
 // Iteration 3: Loops
 // Iteration 3.1
-
-// Solution 1
-const splittedString = hacker1.split('');
-const stringWithSpaces = splittedString.join(' ');
-const upperCaseStringWithSpaces = stringWithSpaces.toUpperCase();
-console.log(upperCaseStringWithSpaces);
-
-// Solution 2
 let driver = "Ola";
 let newDriverName = "";
 
@@ -32,10 +24,15 @@ for (let i = 0; i < driver.length; i++){
 console.log(newDriverName.toUpperCase());
 
 // Iteration 3.2
-function reverse(s){
-    return s.split("").reverse().join("");
+let navigator = "Johnathan";
+let navigatorReversed = "";
+
+for (let i = 0; i < navigator.length; i++){
+  let getChar = navigator.charAt(navigator.length - i);
+  navigatorReversed += getChar;
 }
-console.log(reverse(hacker2));
+navigatorReversed += navigator.charAt(0);
+console.log(navigatorReversed)
 
 // Iteration 3.3
 if (hacker1[0] < hacker2[0]){
@@ -48,7 +45,44 @@ if (hacker1[0] < hacker2[0]){
 
 //BONUS
 
-const stringToCheck = "
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae mauris vitae odio commodo convallis vel quis leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec leo enim, iaculis ut gravida sed, porttitor vel velit. Curabitur at lacinia neque. Nullam convallis mattis orci vitae mollis. Aenean pellentesque felis tincidunt lacus facilisis, eget fringilla enim porta. Maecenas auctor fermentum nisi nec rhoncus.
-Nullam maximus diam ac lacinia tempus. Aenean et congue justo, id tempor neque. Fusce et lacus id lectus faucibus volutpat. Suspendisse auctor, risus id vulputate posuere, ante nulla consectetur diam, in vehicula nisi elit eu odio. Donec imperdiet tincidunt est, id porta nibh varius eu. In ullamcorper imperdiet consequat. Donec a hendrerit felis. Mauris vitae pulvinar massa, ac lobortis tellus. Quisque id nunc consectetur, varius justo non, egestas erat. Ut blandit gravida augue nec varius. Aenean nec tellus pharetra, pulvinar purus ac, fringilla est.
-Sed rhoncus id purus in lobortis. Curabitur tempus erat ut vulputate varius. Aliquam ac ante augue. Cras id felis sapien. Nullam efficitur dolor maximus varius dignissim. Aenean aliquet auctor dolor nec cursus. Curabitur ut quam vitae erat aliquet egestas non a felis. Sed dictum velit sem, sed egestas nisi ultricies eget. Donec nec accumsan nisl. Morbi pharetra interdum faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dictum elit non neque mollis, scelerisque mattis lorem faucibus. "
+const stringToCheck = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae mauris vitae odio commodo convallis vel quis leo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec leo enim, iaculis ut gravida sed, porttitor vel velit. Curabitur at lacinia neque. Nullam convallis mattis orci vitae mollis. Aenean pellentesque felis tincidunt lacus facilisis, eget fringilla enim porta. Maecenas auctor fermentum nisi nec rhoncus. Nullam maximus diam ac lacinia tempus. Aenean et congue justo, id tempor neque. Fusce et lacus id lectus faucibus volutpat. Suspendisse auctor, risus id vulputate posuere, ante nulla consectetur diam, in vehicula nisi elit eu odio. Donec imperdiet tincidunt est, id porta nibh varius eu. In ullamcorper imperdiet consequat. Donec a hendrerit felis. Mauris vitae pulvinar massa, ac lobortis tellus. Quisque id nunc consectetur, varius justo non, egestas erat. Ut blandit gravida augue nec varius. Aenean nec tellus pharetra, pulvinar purus ac, fringilla est. Sed rhoncus id purus in lobortis. Curabitur tempus erat ut vulputate varius. Aliquam ac ante augue. Cras id felis sapien. Nullam efficitur dolor maximus varius dignissim. Aenean aliquet auctor dolor nec cursus. Curabitur ut quam vitae erat aliquet egestas non a felis. Sed dictum velit sem, sed egestas nisi ultricies eget. Donec nec accumsan nisl. Morbi pharetra interdum faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dictum elit non neque mollis, scelerisque mattis lorem faucibus. ";
+
+wordCounter = 0;
+
+for( let i =0; i < stringToCheck.length; i++ ){
+  if (stringToCheck.charAt(i) === " "){
+    wordCounter++;
+  }
+}
+console.log(wordCounter);
+
+
+etCounter = 0;
+
+for( let i =0; i < stringToCheck.length; i++ ){
+  if (stringToCheck.charAt(i) === "e" && stringToCheck.charAt(i + 1) === "t" && stringToCheck.charAt(i + 2) === " "){
+    etCounter++;
+  }
+}
+console.log(etCounter);
+
+
+// Palindrome attempt
+// does not account for commas or spaces :(
+
+
+let palindrome = "racecar"
+
+let palindromeReversed = "";
+ 
+for (let i = 0; i < palindrome.length; i++){
+  let getChar = palindrome.charAt(palindrome.length - i);
+  palindromeReversed += getChar;
+}
+palindromeReversed += palindrome.charAt(0);
+
+if( palindrome === palindromeReversed){
+  console.log("That is a palindrome")
+} else{
+  console.log("That is NOT a palindrome")
+}
